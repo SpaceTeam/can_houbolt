9ca20d0b339641223b97e872a4ff3fa0ee781d62
markuspinter <markus.m.pinter@gmail.com>
2021-03-30 17:32:54 +0200

    add can files

---
+++ channels/digital_out_channel.h
@@ -0,0 +1,44 @@
+#ifndef DIGITAL_OUT_CHANNEL_H__
+#define DIGITAL_OUT_CHANNEL_H__
+
+#include "cmds.h"
+#include "gpio.h"
+#include "measurement.h"
+
+typedef enum
+{
+	DIGITAL_OUT_RESET_SETTINGS,
+	DIGITAL_OUT_SET_STATE,
+
+	DIGITAL_OUT_TOTAL_CMDS
+} DIGITAL_OUT_CMDs;
+
+
+
+typedef struct __attribute__((__packed__))
+{
+	uint8_t state;
+	uint8_t mode;
+}DigialOut_SetStateMsg_t;
+
+
+extern const can_function digital_out_array[];
+
+typedef struct
+{
+	uint8_t channel_index; 		//ToDo: Probably unnecessary
+	Measurement * measurement;
+	uint_least8_t state;
+	uint_least8_t mode;
+	//GPIO_Pin_t * pin;
+}Digital_out_t;
+
+
+Result_t DigitalOut_Init(void *channel, GPIO_Pin_t * enable, GPIO_Pin_t * input);
+Result_t DigitalOut_GetData(void *channel, uint8_t *array, uint8_t *length);
+uint32_t DigitalOut_GetStatus(void *channel);
+
+
+
+
+#endif
---
+++ cmds.h
@@ -0,0 +1,48 @@
+#ifndef INC_CMDS_H__
+#define INC_CMDS_H__
+
+
+#include <stdint.h>
+
+#include "board_config.h"
+
+#define MAX_CHANNELS 32
+
+typedef enum
+{
+	CHANNEL_TYPE_NODE_GENERIC,
+	CHANNEL_TYPE_NODE_SPECIFIC, //?????? Todo: Implement or probably remove
+	CHANNEL_TYPE_ADC16,
+	CHANNEL_TYPE_ADC16_SINGLE,
+	CHANNEL_TYPE_ADC24,
+	CHANNEL_TYPE_DIGITAL_OUT,
+	CHANNEL_TYPE_SERVO,
+
+
+	CHANNEL_TYPE_LAST,
+	CHANNEL_TYPE_UNKNOWN
+} CHANNEL_TYPE;
+
+typedef enum
+{
+	CHANNEL_STATUS_NOICE,
+	CHANNEL_STATUS_TYPE_UNKNOWN,
+	CHANNEL_STATUS_SENSOR_NOT_CONNECTED,
+	CHANNEL_STATUS_WRONG_READINGS,
+	CHANNEL_STATUS_ERROR,
+	CHANNEL_STATUS_NO_CHANNEL
+} CHANNEL_STATUS;
+
+
+
+typedef Result_t (* can_function)( void *, uint32_t *);
+
+
+
+
+
+
+
+
+
+#endif
