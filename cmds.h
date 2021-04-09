538bd0175f238022539d4969703a99eb845791a2
Georg Mikula <georg@mikula.it>
2021-04-09 20:39:49 +0200

    split cmds.h to cmds.h and can_cmds.h renamed generic_cmds.h to generic_channel_def.h

--- cmds.h
+++ cmds.h
@@ -3,8 +3,9 @@
 
 #include <stdint.h>
 
+#include "can_cmds.h"
+
 #define MAX_CHANNELS 32
-#define MAX_DATA_SIZE 64
 typedef enum
 {
 	CHANNEL_TYPE_UNKNOWN,
@@ -24,59 +25,6 @@ typedef enum
 {
 	CHANNEL_STATUS_NOICE, CHANNEL_STATUS_TYPE_UNKNOWN, CHANNEL_STATUS_SENSOR_NOT_CONNECTED, CHANNEL_STATUS_WRONG_READINGS, CHANNEL_STATUS_ERROR, CHANNEL_STATUS_NO_CHANNEL
 } CHANNEL_STATUS;
-
-typedef enum
-{
-	MASTER2NODE_DIRECTION, NODE2MASTER_DIRECTION,
-} CAN_MessageDirection_t;
-
-typedef enum
-{
-	URGENT_PRIORITY, HIGH_PRIORITY, STANDARD_PRIORITY, LOW_PRIORITY
-} CAN_MessagePriority_t;
-
-typedef enum
-{
-	ABORT_SPECIAL_CMD, CLOCK_SYNC_SPECIAL_CMD,							// DIR = MASTER2NODE_DIRECTION
-	ERROR_SPECIAL_CMD = CLOCK_SYNC_SPECIAL_CMD, 	// DIR = NODE2MASTER_DIRECTION
-	INFO_SPECIAL_CMD,
-	STANDARD_SPECIAL_CMD,
-} CAN_MessageSpecialCmd_t;
-
-typedef enum
-{
-	DIRECT_BUFFER, ABSOLUTE_BUFFER, RELATIVE_BUFFER, RESERVED_BUFFER
-} CAN_MessageBuffer_t;
-
-typedef union
-{
-	struct __attribute__((__packed__))
-	{
-		uint32_t direction :1;		//bit:    0   | CAN_MessageDirection_t
-		uint32_t node_id :6;		//bit:  6-1   | Node ID: 0 - 63
-		uint32_t special_cmd :2;	//bit:  8-7   | CAN_MessageSpecialCmd_t
-		uint32_t priority :2;		//bit: 10-9   | CAN_MessagePriority_t
-	} info;
-	uint32_t uint32;
-} Can_MessageId_t;
-
-typedef struct __attribute__((__packed__))
-{
-	uint32_t channel_id :6;					//bit:  5-0   | Channel ID: 0 - 63
-	CAN_MessageBuffer_t buffer :2;			//bit:  7-6   | CAN_MessageBuffer_t
-} Can_MessageDataInfo_t;
-
-typedef union
-{
-	struct __attribute__((__packed__))
-	{
-		Can_MessageDataInfo_t info;
-		uint8_t cmd_id;
-		uint8_t *data;
-	} data;
-	uint8_t uint8[MAX_DATA_SIZE];
-} Can_MessageData_t;
-
 
 
 
