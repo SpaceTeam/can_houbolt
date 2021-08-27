db70388ebab611ba3cb1f219b0b76aad559c66e8
Georg Mikula <georg@mikula.it>
2021-08-27 19:51:03 +0200

    Modified Servo Channel

--- channels/servo_channel_def.h
+++ channels/servo_channel_def.h
@@ -8,15 +8,17 @@
 typedef enum
 {
 	SERVO_POSITION,
+	SERVO_SETPOSITION,
 	SERVO_MAX_SPEED,
-	SERVO_MAX_TORQUE,
 	SERVO_MAX_ACCEL,
-	SERVO_SETPOINT,
+	SERVO_MAX_TORQUE,
 	SERVO_P_PARAM,
 	SERVO_I_PARAM,
 	SERVO_D_PARAM,
 	SERVO_SENSOR_CHANNEL_ID,
-	SERVO_SENSOR_REFRESH_DIVIDER
+	SERVO_SENSOR_REFRESH_DIVIDER,
+	SERVO_POSITION_STARTPOINT,
+	SERVO_POSITION_ENDPOINT
 } SERVO_VARIABLES;
 
 typedef enum
@@ -26,7 +28,7 @@ typedef enum
 	SERVO_SET_VARIABLE,			//SetMsg_t
 	SERVO_GET_VARIABLE,			//SetMsg_t
 	SERVO_ENABLE_CONTROL_LOOP,	//ServoEnableControlLoopMsg_t
-	
+	SERVO_MOVE,	                //ServoMoveMsg_t
 
 	SERVO_TOTAL_CMDS
 } SERVO_CMDs;
@@ -34,9 +36,12 @@ typedef enum
 typedef struct __attribute__((__packed__))
 {
 	uint8_t enable;
-}ServoEnableControlLoopMsg_t;
+} ServoEnableControlLoopMsg_t;
 
-
-
+typedef struct __attribute__((__packed__))
+{
+	uint32_t position;
+	uint32_t interval;	//in ms
+} ServoMoveMsg_t;
 
 #endif
