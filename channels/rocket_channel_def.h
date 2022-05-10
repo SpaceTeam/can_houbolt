953356b2eaa40d8d715d35cf959e5de5bd4c9c4b
markuspinter <markus.m.pinter@gmail.com>
2022-05-10 20:11:58 +0200

    add rocket commands, variables and change names to better describe their behavior

--- channels/rocket_channel_def.h
+++ channels/rocket_channel_def.h
@@ -7,9 +7,11 @@
 
 typedef enum
 {
-	ROCKET_CURRENT_STATE,
-	ROCKET_CHAMBER_PRESSURE_TARGET,
-	ROCKET_REFRESH_DIVIDER
+	ROCKET_MINIMUM_CHAMBER_PRESSURE,
+	ROCKET_MINIMUM_FUEL_PRESSURE,
+	ROCKET_MINIMUM_OX_PRESSURE,
+	ROCKET_HOLDDOWN_TIMEOUT,
+	ROCKET_STATE_REFRESH_DIVIDER
 } ROCKET_VARIABLES;
 
 typedef enum
@@ -22,12 +24,16 @@ typedef enum
 	ROCKET_RES_SET_VARIABLE = COMMON_RES_SET_VARIABLE,		// SetMsg_t
 	ROCKET_REQ_GET_VARIABLE = COMMON_REQ_GET_VARIABLE,		// GetMsg_t
 	ROCKET_RES_GET_VARIABLE = COMMON_RES_GET_VARIABLE,		// SetMsg_t
-	ROCKET_REQ_AUTO_SEQUENCE = COMMON_TOTAL_CMDS,			// NO payload
-	ROCKET_RES_AUTO_SEQUENCE,
+	ROCKET_REQ_SET_ROCKET_STATE = COMMON_TOTAL_CMDS,		// RocketStateMsg_t
+	ROCKET_RES_SET_ROCKET_STATE,		// RocketStateMsg_t
+	ROCKET_REQ_GET_ROCKET_STATE,		// NO payload
+	ROCKET_RES_GET_ROCKET_STATE,		// RocketStateMsg_t
+	ROCKET_REQ_INTERNAL_CONTROL,			// NO payload
+	ROCKET_RES_INTERNAL_CONTROL,
 	ROCKET_REQ_ABORT,
 	ROCKET_RES_ABORT,
-	ROCKET_REQ_ENDOFFLIGHT,
-	ROCKET_RES_ENDOFFLIGHT,
+	ROCKET_REQ_END_OF_FLIGHT,
+	ROCKET_RES_END_OF_FLIGHT,
 
 	ROCKET_TOTAL_CMDS
 } ROCKET_CMDs;
@@ -43,5 +49,24 @@ typedef enum
 	DEPRESS,
 	ABORT
 } ROCKET_STATE;
+
+typedef enum
+{
+	SUCCESS, //returns when set state was successful
+	FAILURE_WRITE_PROTECTED, //returns when set state but write protected
+	WRITABLE, //returns on get state when writable
+	WRITE_PROTECTED //returns on get state when write protected
+} ROCKET_STATE_STATUS;
+
+typedef struct __attribute__((__packed__))
+{
+	uint32_t state; //ROCKET_STATE
+} RocketStateReqMsg_t;
+
+typedef struct __attribute__((__packed__))
+{
+	uint32_t state; //ROCKET_STATE
+	uint32_t status;	//ROCKET_STATE_STATUS
+} RocketStateResMsg_t;
 
 #endif
